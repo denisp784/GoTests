41246fd6d5110df3b0dd71ac63766d89f52dd425
Denis <denisp784@g,ail.com>
2016-02-25 12:22:03 +0300

    fix tests

--- TestReqs/src/main/Main_test.go
+++ TestReqs/src/main/Main_test.go
@@ -20,7 +20,7 @@ func TestReq1(t *testing.T)  {
 	var client Client;
 	err1 := decoder.Decode(&client)
 	checkErr(err1)
-	if (err != nil){
+	if (err != nil || resp.StatusCode != 200){
 		t.Error("code: ", resp.StatusCode)
 	}
 	fmt.Println("Name: ",client.Name, " balance: ", client.Balance);
@@ -29,7 +29,7 @@ func TestReq1(t *testing.T)  {
 func TestReq2(t *testing.T)  {
 	resp1, err := http.PostForm("http://localhost:9000/deposit", url.Values{"user" : {"1"}, "amount" : {"100"}});
 	checkErr(err)
-	if (err != nil){
+	if (err != nil || resp1.StatusCode != 200){
 		t.Error("code: ", resp1.StatusCode)
 	}
 	fmt.Println("code: ", resp1.StatusCode)
@@ -38,7 +38,7 @@ func TestReq2(t *testing.T)  {
 func TestReq3(t *testing.T) {
 	resp2, err := http.PostForm("http://localhost:9000/withdraw", url.Values{"user" : {"1"}, "amount" : {"100"}});
 	checkErr(err)
-	if (err != nil){
+	if (err != nil || resp2.StatusCode != 200){
 		t.Error("code: ", resp2.StatusCode)
 	}
 	fmt.Println("code: ", resp2.StatusCode)
@@ -47,7 +47,7 @@ func TestReq3(t *testing.T) {
 func TestReq4(t *testing.T)  {
 	resp3, err := http.PostForm("http://localhost:9000/transfer", url.Values{"From" : {"1"}, "To" : {"2"}, "amount" : {"100"}});
 	checkErr(err)
-	if (err != nil){
+	if (err != nil || resp3.StatusCode != 200){
 		t.Error("code: ", resp3.StatusCode)
 	}
 	fmt.Println("code: ", resp3.StatusCode)
